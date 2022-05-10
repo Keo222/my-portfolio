206dcaa265a6b19f42f9ffda1827f3df36cfee16
Keo222 <kyote222@gmail.com>
2022-05-10 14:59:52 -0700

    changed box size for form errors

--- src/components/FormAlert.tsx
+++ src/components/FormAlert.tsx
@@ -1,32 +1,32 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
 
 // Styled components
-const ErrorContainer = styled.div<{success: boolean}>`
-    border: 2px solid ${({ success }) => success ? "rgb(47, 245, 47)" : "rgb(255, 0, 0)" };
-    border-radius: 5px;
-    background-color: ${({ success }) => success ? "rgba(47, 245, 47, 0.2)" : "rgba(255, 0, 0, 0.2)" };
-    width: 100%;
-    margin-top: 0.5rem;
-`
+const ErrorContainer = styled.div<{ success: boolean }>`
+  border: 2px solid
+    ${({ success }) => (success ? "rgb(47, 245, 47)" : "rgb(255, 0, 0)")};
+  border-radius: 5px;
+  background-color: ${({ success }) =>
+    success ? "rgba(47, 245, 47, 0.2)" : "rgba(255, 0, 0, 0.2)"};
+  margin-top: 0.5rem;
+`;
 const ErrorMessage = styled.p`
-font-size: 1.4rem;
-    margin-block: 0;
-    margin-left: 0.5rem;
-`
-
+  font-size: 1.4rem;
+  margin-block: 0;
+  margin-left: 0.5rem;
+`;
 
 type Props = {
-    alertMsg: string
-    alertType: "success" | "error"
-}
+  alertMsg: string;
+  alertType: "success" | "error";
+};
 
-const FormAlert = ({alertMsg, alertType}: Props) => {
+const FormAlert = ({ alertMsg, alertType }: Props) => {
   return (
     <ErrorContainer success={alertType === "success"}>
-        <ErrorMessage>{alertMsg}</ErrorMessage>
+      <ErrorMessage>{alertMsg}</ErrorMessage>
     </ErrorContainer>
-  )
-}
+  );
+};
 
-export default FormAlert
+export default FormAlert;
