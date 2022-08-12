75b0309641e8fa48c54e6f738f571e10900f35aa
Kyle Olsen <kyle.olsen222@gmail.com>
2022-08-12 14:46:27 -0700

    small code layout change on About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -11,7 +11,6 @@ import github from "../images/logos/github-black.svg";
 import linkedin from "../images/logos/linkedin_logo.svg";
 
 // Styled Components
-
 const AboutPageDiv = styled.div`
   display: flex;
   justify-content: space-around;
@@ -144,7 +143,7 @@ const AboutPicsSection = styled.section`
 
 const About = () => {
   return (
-    <AboutPageDiv>
+    <>
       <Helmet>
         <title>Kyle Olsen: About</title>
         <meta
@@ -152,50 +151,47 @@ const About = () => {
           content="Hi! My name is Kyle Olsen and I am a web developer in the Portland, Oregon area. I am a fullstack web developer focusing on React.js, Node.js, Express.js, and PostgreSQL. Read more about me here."
         />
       </Helmet>
-      <AboutInfoSection aria-labelledby="about-header">
-        <AboutHeader id="about-header">About</AboutHeader>
-        <AboutInfoDiv>
-          <AboutInfoP>
-            Hi! My name is Kyle Olsen and I was born and raised in Oregon.
-            I have a Bachelor’s of Music from Rice University in Houston,
-            TX and I also attended The Juilliard School while pursuing a
-            Master’s of Music degree. Currently, I'm building websites as a
-            self-taught web developer while continuing to play music
-            regularly with the Oregon Symphony, the Portland Opera, and the
-            Oregon Ballet Theater. I hope to find a fulltime career in web
-            development.
-          </AboutInfoP>
-          <AboutInfoP>
-            In my free time I enjoy running, gardening, and spending time
-            outdoors.
-          </AboutInfoP>
-        </AboutInfoDiv>
-        <SocialsDiv>
-          <SocialLink
-            href="https://github.com/Keo222"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <SocialLogo src={github} title="Github" alt="Github Logo" />
-          </SocialLink>
-          <SocialLink
-            href="https://www.linkedin.com/in/kyle-olsen-2958084b/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <SocialLogo
-              src={linkedin}
-              title="LinkedIn"
-              alt="LinkedIn Logo"
-            />
-          </SocialLink>
-        </SocialsDiv>
-        <ContactButton to="/contact">Contact Me</ContactButton>
-      </AboutInfoSection>
-      <AboutPicsSection aria-label="Pictures of Kyle Olsen">
-        <ImageCarousel images={personalImages} />
-      </AboutPicsSection>
-    </AboutPageDiv>
+      <AboutPageDiv>
+        <AboutInfoSection aria-labelledby="about-header">
+          <AboutHeader id="about-header">About</AboutHeader>
+          <AboutInfoDiv>
+            <AboutInfoP>
+              Hi! My name is Kyle Olsen and I was born and raised in Oregon. I
+              have a Bachelor’s of Music from Rice University in Houston, TX and
+              I also attended The Juilliard School while pursuing a Master’s of
+              Music degree. Currently, I'm building websites as a self-taught
+              web developer while continuing to play music regularly with the
+              Oregon Symphony, the Portland Opera, and the Oregon Ballet
+              Theater. I hope to find a fulltime career in web development.
+            </AboutInfoP>
+            <AboutInfoP>
+              In my free time I enjoy running, gardening, and spending time
+              outdoors.
+            </AboutInfoP>
+          </AboutInfoDiv>
+          <SocialsDiv>
+            <SocialLink
+              href="https://github.com/Keo222"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <SocialLogo src={github} title="Github" alt="Github Logo" />
+            </SocialLink>
+            <SocialLink
+              href="https://www.linkedin.com/in/kyle-olsen-2958084b/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <SocialLogo src={linkedin} title="LinkedIn" alt="LinkedIn Logo" />
+            </SocialLink>
+          </SocialsDiv>
+          <ContactButton to="/contact">Contact Me</ContactButton>
+        </AboutInfoSection>
+        <AboutPicsSection aria-label="Pictures of Kyle Olsen">
+          <ImageCarousel images={personalImages} />
+        </AboutPicsSection>
+      </AboutPageDiv>
+    </>
   );
 };
 
