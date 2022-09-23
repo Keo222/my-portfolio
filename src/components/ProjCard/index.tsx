aedb25ae8dfc0efea6218ec1b9c42186a9877851
Kyle Olsen <kyle.olsen222@gmail.com>
2022-09-23 13:09:05 -0700

    moved ProjCard styles to their own file

--- src/components/ProjCard/index.tsx
+++ src/components/ProjCard/index.tsx
@@ -1,55 +1,13 @@
-import styled from "styled-components";
-
 // Styled Components
-const ProjArticle = styled.article<{ bgColor: boolean }>`
-  margin-bottom: 8rem;
-  padding: 5rem 4rem;
-  /* border: 2px solid ${(props) => props.theme.color.highlight1}; */
-  border-radius: 10px;
-  background-color: ${({ bgColor }) =>
-    bgColor === true ? "#f2f7f1" : "#f1eff6"};
-`;
+import {
+  ProjArticle,
+  ImgDiv,
+  ProjImg,
+  TechUsedText,
+  ProjTitle,
+  AboutProjText,
+} from "./styledProjCard";
 
-const ImgDiv = styled.div`
-  margin-inline: auto;
-  width: clamp(200px, 75%, 800px);
-`;
-
-const ProjImg = styled.img`
-  object-fit: contain;
-  width: 100%;
-  aspect-ratio: 1.777777777777777777777;
-`;
-
-const TechUsedText = styled.p`
-  text-align: center;
-  font-size: 1.3rem;
-`;
-
-const ProjTitle = styled.h3<{ mainHighlight: boolean }>`
-  font-size: 2rem;
-  font-weight: 400;
-  margin-block: 2rem;
-  transition: color 0.1s;
-
-  & > a {
-    color: inherit;
-  }
-
-  & > a:hover {
-    color: ${(props) =>
-      props.mainHighlight === true
-        ? props.theme.color.highlight1
-        : props.theme.color.highlight2};
-  }
-`;
-
-const AboutProjText = styled.p`
-  font-size: 1.6rem;
-  line-height: 1.8em;
-  width: clamp(240px, 80%, 700px);
-  margin-inline: auto;
-`;
 type Props = {
   mainHighlight: boolean;
   projName: string;
