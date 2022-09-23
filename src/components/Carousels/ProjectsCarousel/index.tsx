ea7e5220c2605ef2b422a46f5031738d204600a1
Kyle Olsen <kyle.olsen222@gmail.com>
2022-09-23 12:58:48 -0700

    moved ProjectsCarousel styles to their own file

--- src/components/Carousels/ProjectsCarousel/index.tsx
+++ src/components/Carousels/ProjectsCarousel/index.tsx
@@ -1,6 +1,5 @@
 import { useEffect, KeyboardEvent } from "react";
-import styled from "styled-components";
-import { animated, useSpringRef, useTransition } from "react-spring";
+import { useSpringRef, useTransition } from "react-spring";
 
 // Components
 import CarouselTicks from "../CarouselTicks";
@@ -21,149 +20,21 @@ import svelte_logo from "images/logos/svelte_logo.svg";
 // Types
 import { ProjArray } from "../../../customTypes";
 
-const SliderSection = styled.section`
-  width: 100%;
-  height: 30vw;
-
-  margin-top: 1rem;
-  margin-bottom: 2rem;
-
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  position: relative;
-
-  @media screen and (${(props) => props.theme.responsive.lg}) {
-    height: fit-content;
-  }
-`;
-
-const LeftArrow = styled.img`
-  height: 5rem;
-
-  margin-inline: 5px;
-
-  vertical-align: bottom;
-  position: absolute;
-  z-index: 15;
-
-  user-select: none;
-  opacity: 0.4;
-  transition: all 0.15s;
-
-  &:hover,
-  &:focus,
-  &:focus-visible {
-    cursor: pointer;
-    opacity: 1;
-  }
-`;
-
-const RightArrow = styled(LeftArrow)`
-  right: 0;
-`;
-
-const ExternalLink = styled.a`
-  position: absolute;
-  top: 2rem;
-  right: 2rem;
-
-  &:focus img,
-  &:focus-visible img {
-    opacity: 1;
-  }
-`;
-const ExternalLinkImg = styled.img`
-  height: 3rem;
-  width: 3rem;
-
-  opacity: 0.4;
-  transition: all 0.15s;
-
-  &:hover {
-    cursor: pointer;
-    opacity: 1;
-  }
-`;
-
-const InfoDiv = styled(animated.div)`
-  display: flex;
-  align-items: center;
-
-  height: 100%;
-  width: 100%;
-
-  @media screen and (${(props) => props.theme.responsive.lg}) {
-    flex-direction: column;
-  }
-`;
-
-const SiteImg = styled.img`
-  object-fit: cover;
-  height: 100%;
-  aspect-ratio: 1.7777777;
-  @media screen and (${(props) => props.theme.responsive.lg}) {
-    width: 100%;
-    height: auto;
-  }
-`;
-
-const InfoTextDiv = styled.div`
-  height: 100%;
-
-  display: flex;
-  flex-direction: column;
-  flex-grow: 1;
-  align-items: center;
-  justify-content: space-around;
-
-  color: ${(props) => props.theme.color.primary1};
-  background-color: ${(props) => props.theme.color.primary2};
-
-  @media screen and (${(props) => props.theme.responsive.lg}) {
-    width: 100%;
-    padding-block: 3rem;
-  }
-`;
-
-const SiteHeader = styled.h3`
-  font-family: "Montserrat", sans-serif;
-  font-size: 2.4rem;
-  font-weight: 800;
-  letter-spacing: 4px;
-  text-align: center;
-`;
-
-const SiteDescription = styled.p`
-  font-size: 1.5rem;
-  font-weight: 400;
-  text-align: center;
-  line-height: 2;
-  color: #cbcbcb;
-
-  width: 65%;
-
-  @media screen and (${(props) => props.theme.responsive.lg}) {
-    margin-block: 5rem;
-  }
-`;
-
-const TechImgsDiv = styled.div`
-  width: clamp(200px, 80%, 500px);
-
-  margin-top: 2rem;
-
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
-`;
-
-const TechLogo = styled.img`
-  height: 5rem;
-  width: 5rem;
-  margin-inline: 1rem;
-`;
+// Styled Components
+import {
+  SliderSection,
+  LeftArrow,
+  RightArrow,
+  InfoDiv,
+  SiteImg,
+  InfoTextDiv,
+  SiteHeader,
+  SiteDescription,
+  TechImgsDiv,
+  TechLogo,
+  ExternalLink,
+  ExternalLinkImg,
+} from "./styledProjectCarousel";
 
 type Props = {
   projects: ProjArray;
@@ -265,14 +136,10 @@ const ProjectsCarousel = ({
   }, [currentProj, transRef]);
   // Navigate Projects
   const nextProject = () => {
-    setCurrentProj(
-      currentProj === projects.length - 1 ? 0 : currentProj + 1
-    );
+    setCurrentProj(currentProj === projects.length - 1 ? 0 : currentProj + 1);
   };
   const prevProject = () => {
-    setCurrentProj(
-      currentProj === 0 ? projects.length - 1 : currentProj - 1
-    );
+    setCurrentProj(currentProj === 0 ? projects.length - 1 : currentProj - 1);
   };
 
   const keyboardHandleNav = (
@@ -306,43 +173,36 @@ const ProjectsCarousel = ({
             <InfoDiv data-testid="slide-testId" style={styles} key={i}>
               <SiteImg
                 srcSet={`${
-                  projects[
-                    i <= projects.length - 1 ? i : projects.length - 1
-                  ].imgs[600]
+                  projects[i <= projects.length - 1 ? i : projects.length - 1]
+                    .imgs[600]
                 } 600w,${
-                  projects[
-                    i <= projects.length - 1 ? i : projects.length - 1
-                  ].imgs[900]
+                  projects[i <= projects.length - 1 ? i : projects.length - 1]
+                    .imgs[900]
                 } 900w,
                 ${
-                  projects[
-                    i <= projects.length - 1 ? i : projects.length - 1
-                  ].imgs[1200]
+                  projects[i <= projects.length - 1 ? i : projects.length - 1]
+                    .imgs[1200]
                 } 1200w,`}
                 src={
-                  projects[
-                    i <= projects.length - 1 ? i : projects.length - 1
-                  ].imgs[1200]
+                  projects[i <= projects.length - 1 ? i : projects.length - 1]
+                    .imgs[1200]
                 }
                 alt={
-                  projects[
-                    i <= projects.length - 1 ? i : projects.length - 1
-                  ].name
+                  projects[i <= projects.length - 1 ? i : projects.length - 1]
+                    .name
                 }
               />
               <InfoTextDiv>
                 <SiteHeader>
                   {
-                    projects[
-                      i <= projects.length - 1 ? i : projects.length - 1
-                    ].name
+                    projects[i <= projects.length - 1 ? i : projects.length - 1]
+                      .name
                   }
                 </SiteHeader>
                 <SiteDescription>
                   {
-                    projects[
-                      i <= projects.length - 1 ? i : projects.length - 1
-                    ].description
+                    projects[i <= projects.length - 1 ? i : projects.length - 1]
+                      .description
                   }
                 </SiteDescription>
                 <TechImgsDiv>
@@ -354,9 +214,8 @@ const ProjectsCarousel = ({
                   target="_blank"
                   rel="noreferrer noopener"
                   href={
-                    projects[
-                      i <= projects.length - 1 ? i : projects.length - 1
-                    ].link
+                    projects[i <= projects.length - 1 ? i : projects.length - 1]
+                      .link
                   }
                 >
                   <ExternalLinkImg
